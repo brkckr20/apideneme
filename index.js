27fd2ad2d03607b2aa0dd4bfc992b7018be55009
Burak <burak2043@gmail.com>
2022-03-08 13:42:03 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config()
+const PORT = process.env.PORT;
+
+const list = [
+    {
+        id: 1,
+        name: "burak"
+    }
+]
+
+app.get("/",(req,res)=>{
+    res.status(200).send(list)
+    console.log(list)
+})
+
+console.log(list);
+app.listen(PORT,() => {
+    console.log(`server ${PORT} nolu porttan hizmette`)
+})
